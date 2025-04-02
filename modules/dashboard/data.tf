6256011583326e931de291f71b4123571c8e8dfb
juergen.vanzeir@swisscom.com <juergen.vanzeir@swisscom.com>
2025-04-02 11:35:47 +0200

    Add modules/dashboard/data.tf [skip ci]

---
+++ modules/dashboard/data.tf
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+/*
+SUMMARY : This file contains the data block to get Azure domains
+AUTHOR/S: Cloud for Industry
+*/
+# This allows us to get the tenant id
+data "azurerm_client_config" "current" {}
